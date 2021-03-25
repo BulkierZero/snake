69695529944d943bbea513666dfe187cb59dba91
BulkierZero <81317124+BulkierZero@users.noreply.github.com>
2021-03-24 23:31:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,25 +18,21 @@ def end_check():
         if snake.x[0] == snake.x[i] and snake.y[0] == snake.y[i]:
             global running
             running = False
-            print("Game Over Noob! " + "Score:" + str(snake.length))
+            print("Get Good Noob! " + "Score:" + str(snake.length))
 
 class Snake:    # the snake!
     def __init__(self):
         self.length = 1    # length of snake array
         self.speed = 40    # speed in pixels
-<<<<<<< Updated upstream
-        self.x = [360] * self.length    # position of snake in x
-        self.y = [360] * self.length    # position of snake in y
-=======
         self.x = [(screen_width/2) - (size/2)]    # position of snake in x
         self.y = [(screen_height/2) - (size/2)]    # position of snake in y
->>>>>>> Stashed changes
         self.dir_x = 0    # direction snake in x
         self.dir_y = 0    # direction of snake in y
 
     def draw(self):    # draws the snake to surface.
         for i in range(self.length):    # its supposed to run through the snake array and draw each x and y coordinate.
-            pygame.draw.rect(screen, (255, 0, 0), [self.x[i], self.y[i], size-1, size-1])
+            pygame.draw.rect(screen, (255, 0, 0), [self.x[0], self.y[0], size, size])
+            pygame.draw.rect(screen, (120, 0, 0), [self.x[i], self.y[i], size, size])
 
     def move(self):    # changes x and y coordinates of snake
         for i in range(self.length-1, 0, -1):    # loop through array backwards changing next element to current
@@ -45,39 +41,25 @@ class Snake:    # the snake!
 
         self.x[0] = self.x[0] + (self.speed * self.dir_x)    # takes the position of head and adds the vector to x
         self.y[0] = self.y[0] + (self.speed * self.dir_y)    # takes the position of head and adds the vector to y
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-        for i in range(1, self.length, 1):    # loop through array changing previous element value to current value...maybe
-            self.x[i] = self.x[i-1] - self.x[i]
-            self.y[i] = self.y[i-1]  - self.y[i]
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         # wraps snake from one side of screen to the other in both x and y
-        if self.x == screen_width:
-            self.x = 0
-        elif self.x == -size:
-            self.x = screen_width - size
-        elif self.y == screen_height:
-            self.y = 0
-        elif self.y == -size:
-            self.y = screen_height - size
+        if self.x[0] == screen_width:
+            self.x[0] = 0
+        elif self.x[0] == -size:
+            self.x[0] = screen_width - size
+        elif self.y[0] == screen_height:
+            self.y[0] = 0
+        elif self.y[0] == -size:
+            self.y[0] = screen_height - size
 
     def grow(self):    # increases the x and y position arrays by 1
         self.length += 1
-
+        self.x.append(self.x[-1])
+        self.y.append(self.y[-1])
+        
 
 class Food:    # makes the foods
     def __init__(self):    # initial x and y coordinates
         self.x = random.randrange(0, screen_width - size, size)
-<<<<<<< Updated upstream
-        self.y = random.randrange(0, screen_height, size)
-        
-    def new_location(self):
-        self.x = random.randrange(0, 760, 40)
-        self.y = random.randrange(0, 760, 40)
-=======
         self.y = random.randrange(0, screen_height - size, size)
         
     def new_location(self):
@@ -86,7 +68,6 @@ class Food:    # makes the foods
         for i in range(snake.length):
             if snake.x[i] == self.x and snake.y[i] == self.y:
                 self.new_location()
->>>>>>> Stashed changes
 
     def draw(self):
         pygame.draw.rect(screen, (0, 255, 0), [self.x, self.y, size, size])
